6d3730b680951b2e040afd6d09c505af3f4b080c
Venkata Kanna Bhavan Surya Adapa <avkbsurya@gmail.com>
2026-09-20 13:12:32 +0530

    Enhance fruit options retrieval and processing
    Added SEARCH_ON column to fruit options and converted Snowpark DataFrame to Pandas for easier data manipulation.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -20,12 +21,18 @@ st.write("The name on your Smoothie will be:", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-# Get fruit names from Snowflake
+# Get fruit names and search values from Snowflake
 my_dataframe = session.table(
     "smoothies.public.fruit_options"
-).select(col("FRUIT_NAME"))
+).select(
+    col("FRUIT_NAME"),
+    col("SEARCH_ON")
+)
 
-# Choose up to 5 ingredients
+# Convert the Snowpark DataFrame to a Pandas DataFrame
+# so we can use the loc function
+pd_df = my_dataframe.to_pandas()
+
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
     my_dataframe,
@@ -36,7 +43,20 @@ if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
-        ingredients_string = ingredients_string + fruit_chosen + ' '
+        ingredients_string += fruit_chosen + ' '
+
+        search_on = pd_df.loc[
+            pd_df['FRUIT_NAME'] == fruit_chosen,
+            'SEARCH_ON'
+        ].iloc[0]
+
+        st.write(
+            'The search value for ',
+            fruit_chosen,
+            ' is ',
+            search_on,
+            '.'
+        )
 
         st.subheader(fruit_chosen + ' Nutrition Information')
 
