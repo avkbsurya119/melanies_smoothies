f4a7e0c33317fcc03e3f3122ebb2a7b77f1b614d
Venkata Kanna Bhavan Surya Adapa <avkbsurya@gmail.com>
2026-09-20 12:58:22 +0530

    Fix string concatenation and add subheader for fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,10 +36,12 @@ if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
+        ingredients_string = ingredients_string + fruit_chosen + ' '
+
+        st.subheader(fruit_chosen + ' Nutrition Information')
 
         smoothiefroot_response = requests.get(
-            "https://my.smoothiefroot.com/api/fruit/watermelon"
+            "https://my.smoothiefroot.com/api/fruit/" + fruit_chosen
         )
 
         sf_df = st.dataframe(
